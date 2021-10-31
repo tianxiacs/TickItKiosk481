bae0558ce201918c4333f4e8af25a0bcb938baf4
Tian Xia <56200169+tianxiahci@users.noreply.github.com>
2021-10-30 19:01:25 -0600

    ticket and seat selection

---
+++ TickItKiosk481/SelectSeat.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TickItKiosk481
+{
+    /// <summary>
+    /// Interaction logic for SelectSeat.xaml
+    /// </summary>
+    public partial class SelectSeat : Page
+    {
+        public SelectSeat()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TickItKiosk481/SelectTicket.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TickItKiosk481
+{
+    /// <summary>
+    /// Interaction logic for SelectTicket.xaml
+    /// </summary>
+    public partial class SelectTicket : Page
+    {
+        public SelectTicket()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
