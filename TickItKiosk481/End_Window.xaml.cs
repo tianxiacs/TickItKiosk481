f1ec85edd85ee3b0e1341bae123ffdd87b10532d
shehzan31 <47336141+shehzan31@users.noreply.github.com>
2021-11-18 13:34:41 -0700

    Changed few errors

---
+++ TickItKiosk481/End_Window.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TickItKiosk481
+{
+    /// <summary>
+    /// Interaction logic for End_Window.xaml
+    /// </summary>
+    public partial class End_Window : Window
+    {
+        public End_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TickItKiosk481/Payment_Window.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TickItKiosk481
+{
+    /// <summary>
+    /// Interaction logic for Payment_Window.xaml
+    /// </summary>
+    public partial class Payment_Window : Window
+    {
+        public Payment_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
