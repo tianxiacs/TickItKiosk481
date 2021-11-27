5d368c5d45e132e7a152dc5304797d7ca8f14817
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2021-11-26 20:15:39 -0700

    Yuan's work
    Finish some button clicks before time selection

---
+++ TickItKiosk481/TickItKiosk481/Payment.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TickItKiosk481
+{
+    /// <summary>
+    /// Interaction logic for Payment_Window.xaml
+    /// </summary>
+    public partial class Payment : Window
+    {
+        public Payment()
+        {
+            InitializeComponent();
+        }
+
+        private void CreditDebitPayment(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            App.cdp.Show();
+        }
+
+        private void GiftCardPayment(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            App.giftPayment.Show();
+        }
+    }
+}
---
+++ TickItKiosk481/TickItKiosk481/Ticket.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TickItKiosk481
+{
+    /// <summary>
+    /// Interaction logic for Ticket.xaml
+    /// </summary>
+    public partial class Ticket : Window
+    {
+        public static double subtotal = 0.0;
+        public static int adultNum = 0;
+        public static int childNum = 0;
+        public static int seniorNum = 0;
+        public static int redeemNum = 0;
+
+        public Ticket()
+        {
+            InitializeComponent();
+        }
+
+        private void GetLanguage(object sender, RoutedEventArgs e)
+        {
+            LanguageScreen.lastPage = this;
+            this.Visibility = Visibility.Hidden;
+            App.languageScreen.Show();
+        }
+
+        private void GetHelp(object sender, RoutedEventArgs e)
+        {
+            Help.lastPage = this;
+            this.Visibility = Visibility.Hidden;
+            App.help.Show();
+        }
+
+        private void AddAdult(object sender, RoutedEventArgs e)
+        {
+            adultNum++;
+            AdultLabel.Content = adultNum;
+        }
+
+        private void AddChild(object sender, RoutedEventArgs e)
+        {
+            childNum++;
+            ChildLabel.Content = childNum;
+        }
+
+        private void AddSenior(object sender, RoutedEventArgs e)
+        {
+            seniorNum++;
+            SeniorLabel.Content = seniorNum;
+        }
+
+        private void AddRedeem(object sender, RoutedEventArgs e)
+        {
+            redeemNum++;
+            RedeemLabel.Content = redeemNum;
+        }
+
+        private void MinusAdult(object sender, RoutedEventArgs e)
+        {
+            adultNum--;
+            AdultLabel.Content = adultNum;
+        }
+
+        private void MinusChild(object sender, RoutedEventArgs e)
+        {
+            childNum--;
+            ChildLabel.Content = childNum;
+        }
+
+        private void MinusSenior(object sender, RoutedEventArgs e)
+        {
+            seniorNum--;
+            SeniorLabel.Content = seniorNum;
+        }
+
+        private void MinusRedeem(object sender, RoutedEventArgs e)
+        {
+            redeemNum--;
+            RedeemLabel.Content = redeemNum;
+        }
+
+        private void GoToSeats(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            App.seat.Show();
+        }
+
+    }
+}
